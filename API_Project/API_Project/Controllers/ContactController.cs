059d2f4e4ca73bda9705b1e8d3c38db8db3381af
NhienNguyen <nguyentrinhien15@gmail.com>
2025-03-21 15:49:18 +0700

    API Contact

---
+++ API_Project/API_Project/Controllers/ContactController.cs
@@ -0,0 +1,96 @@
+﻿using API_Project.Data;
+using API_Project.DTOs;
+using API_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace API_Project.Controllers
+    
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ContactController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public ContactController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        //Get: api/contact
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Contact>>> GetContacts()
+        {
+            return await _context.Contacts.ToListAsync();
+        }
+
+        // GET: api/Contact/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Contact>> GetContact(int id)
+        {
+            var contact = await _context.Contacts.FindAsync(id);
+
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return contact;
+        }
+
+        // POST: api/Contact
+        [HttpPost]
+        public async Task<ActionResult<Contact>> PostContact(ContactDTO contactDTO)
+        {
+            var contact = new Contact
+            {
+                Fullname = contactDTO.Fullname,
+                Email = contactDTO.Email,
+                Phonenumber = contactDTO.Phonenumber,
+                Description = contactDTO.Description,
+                Title = contactDTO.Title,
+                CreatedAt = DateTime.Now,
+                IsRead = false
+            };
+
+            _context.Contacts.Add(contact);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetContact), new { id = contact.Id }, contact);
+        }
+
+        // PUT: api/Contact/5/read
+        [HttpPut("{id}/read")]
+        public async Task<IActionResult> MarkAsRead(int id)
+        {
+            var contact = await _context.Contacts.FindAsync(id);
+
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            contact.IsRead = true;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE: api/Contact/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteContact(int id)
+        {
+            var contact = await _context.Contacts.FindAsync(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            _context.Contacts.Remove(contact);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+    }
+}
